fe6543fed7ea7d5ba1f9486a3fbc579d52a2bce4
Anyh Salinas <anelise.salinas96@hotmail.com>
2023-07-29 17:56:24 -0300

    Update anyhs-theme-editor.js

--- anyhs-theme-editor.js
+++ anyhs-theme-editor.js
@@ -1,20 +1,15 @@
-//alert("{color:Background}");
-
-var uConfigs = document.querySelector("#uConfigs").value;
-alert(uConfigs);
-
-    document.querySelector("#render-custom-theme").onclick=function(){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onload = function() {
-            let code = xhttp.responseText;
-            var customCode = code.replace(/{color:Background}/g, "#000000");
-            const link = document.createElement("a");
-            var file = new File([customCode], { type: 'text/plain' });
-            link.href = URL.createObjectURL(file);
-            link.download = "custom-anyhs-theme-002.txt";
-            link.click();
-            URL.revokeObjectURL(link.href);
-        }; // onload
-        xhttp.open("GET", "https://raw.githubusercontent.com/anyhsalinas/free-codes/main/tb-themes/theme002.txt", true);
-        xhttp.send();
-    }; // onclick
+document.querySelector("#render-custom-theme").onclick=function(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        let code = xhttp.responseText;
+        var customCode = code.replace(/{color:Background}/g, "#000000");
+        const link = document.createElement("a");
+        var file = new File([customCode], { type: 'text/plain' });
+        link.href = URL.createObjectURL(file);
+        link.download = themeName;
+        link.click();
+        URL.revokeObjectURL(link.href);
+    }; // onload
+    xhttp.open("GET", themeCode, true);
+    xhttp.send();
+}; // onclick
