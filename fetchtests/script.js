ff112b4247fe80a02734453636061af60067bff0
Anyh Salinas <anyhsalinas@outlook.com>
2024-12-11 03:19:55 -0300

    Update script.js

--- fetchtests/script.js
+++ fetchtests/script.js
@@ -1,7 +1,7 @@
 document.querySelector("div").style.border="2px solid blue";
 
 $(document).ready(function(){
-  $("div").load("post1.html", function(responseTxt, statusTxt, xhr){
+  $("div").load("./post1.html", function(responseTxt, statusTxt, xhr){
     if(statusTxt == "success")
       alert("External content loaded successfully!");
     if(statusTxt == "error")
