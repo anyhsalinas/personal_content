af70ca85930ba061feaa1656135d4fa24f98f0ff
Anyh Salinas <anyhsalinas@outlook.com>
2024-12-11 03:16:12 -0300

    Updated script.js

--- fetchtests/script.js
+++ fetchtests/script.js
@@ -1,3 +1,12 @@
 document.querySelector("div").style.border="2px solid red";
 
+$(document).ready(function(){
+  $("div").load("post1.html", function(responseTxt, statusTxt, xhr){
+    if(statusTxt == "success")
+      alert("External content loaded successfully!");
+    if(statusTxt == "error")
+      alert("Error: " + xhr.status + ": " + xhr.statusText);
+  });
+});
+
 alert("yeah");
